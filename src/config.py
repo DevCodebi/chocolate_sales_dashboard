d6d9e6abb3418ff75abfedb5be11e0f986ad8bc3
DevCodebi <elcksoncs@hotmail.com>
2026-05-02 00:29:16 -0300

    inserção dos dados do google sheets

--- src/config.py
+++ src/config.py
@@ -1,8 +1,14 @@
 from pathlib import Path
 
-# raiz do projeto = pasta onde está o requirements.txt
+# raiz do projeto
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 
 DATA_DIR = PROJECT_ROOT / "data"
 OUTPUT_DIR = PROJECT_ROOT / "outputs"
 NOTEBOOKS_DIR = PROJECT_ROOT / "notebooks"
+
+# credenciais
+CREDENTIALS_PATH = PROJECT_ROOT / "credentials" / "api_google_credentials.json"
+
+# Google Sheets
+SHEET_ID = "1IdJH7ZP7hlmwWEFAnQtqq-Tz_Mq-47slWqi2j-VjLvU"
--- src/io.py
+++ src/io.py
@@ -1,15 +1,43 @@
 from pathlib import Path
 import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
+
 
 def load_data(path: str | Path) -> pd.DataFrame:
     path = Path(path)
     suffix = path.suffix.lower()
 
-    if suffix in [".csv"]:
+    if suffix == ".csv":
         return pd.read_csv(path)
     if suffix in [".xlsx", ".xls"]:
         return pd.read_excel(path)
-    if suffix in [".parquet"]:
+    if suffix == ".parquet":
         return pd.read_parquet(path)
 
     raise ValueError(f"Formato não suportado: {suffix}")
+
+
+def load_google_sheet(
+    sheet_id: str,
+    worksheet_name: str,
+    credentials_path: str | Path
+) -> pd.DataFrame:
+
+    scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+
+    creds = Credentials.from_service_account_file(
+        str(credentials_path),  # garante compatibilidade com Path
+        scopes=scopes
+    )
+
+    client = gspread.authorize(creds)
+
+    sheet = client.open_by_key(sheet_id)
+    worksheet = sheet.worksheet(worksheet_name)
+
+    data = worksheet.get_all_records()
+
+    df = pd.DataFrame(data)
+
+    return df
